118427c3505f753cf0d64ca0b37278a93e7fd5a3
Steinar Bang <sb@dod.no>
2013-08-13 14:59:24 +0200

    Initial checkin.

---
+++ src/main/java/no/steria/sbang/chinookextractor/App.java
@@ -0,0 +1,58 @@
+package no.steria.sbang.chinookextractor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+    
+        String url = "jdbc:postgresql://localhost/Chinook";
+        String user = "chinook";
+        String password = "klrp320";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(App.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(App.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }    }
+}
---
+++ src/test/java/no/steria/sbang/chinookextractor/AppTest.java
@@ -0,0 +1,21 @@
+package no.steria.sbang.chinookextractor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+	public void testApp()
+    {
+        assertTrue( true );
+    }
+}
