79cdca985ad555fc0a950f60e007196bf8ba0f1f
Steinar Bang <sb@dod.no>
2013-08-15 16:02:21 +0200

    Conversion from JDBC result set to JSON with basic unit test.
    Should have been corner case testing as well, but no time for it now.

---
+++ src/test/java/no/steria/sbang/chinookextractor/ResultSetConverterTest.java
@@ -0,0 +1,47 @@
+package no.steria.sbang.chinookextractor;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.mockrunner.base.BaseTestCase;
+import com.mockrunner.mock.jdbc.MockResultSet;
+
+public class ResultSetConverterTest extends BaseTestCase {
+
+	@Test
+	public void testConvert() throws SQLException, JSONException {
+		MockResultSet mockResultSet = new MockResultSet("resultsetId");
+		mockResultSet.addColumn("Name");
+		mockResultSet.addColumn("Address");
+		mockResultSet.addColumn("City");
+		mockResultSet.addColumn("Country");
+		List<String> row1 = new ArrayList<String>();
+		row1.add("John Doe");
+		row1.add("Unknown street 101");
+		row1.add("Los Angeles");
+		row1.add("USA");
+		mockResultSet.addRow(row1);
+		List<String> row2 = new ArrayList<String>();
+		row2.add("Arne Andsen");
+		row2.add("Inkognitogata 10");
+		row2.add("Andebu");
+		row2.add("Norge");
+		mockResultSet.addRow(row2);
+		JSONArray json = ResultSetConverter.convert(mockResultSet);
+		assertEquals(2, json.length());
+		JSONObject firstElement = (JSONObject)(json.get(0)); 
+		assertEquals(4, firstElement.length());
+		JSONObject secondElement = (JSONObject)(json.get(1)); 
+		assertEquals(4, secondElement.length());
+	}
+
+}
